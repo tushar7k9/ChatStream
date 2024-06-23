64c403cd6801f1562aa2e41afb0c9613637976f8
Tushar <tushar@Tushars-MacBook-Pro.local>
2024-06-23 14:50:18 +0530

    add theme provider(dark/light mode)

--- chatstream/app/(main)/(routes)/page.tsx
+++ chatstream/app/(main)/(routes)/page.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { UserButton } from '@clerk/nextjs';
+import ThemeToggle from "@/components/theme-toggle";
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <UserButton afterSignOutUrl="/sign-in" />
+      <ThemeToggle />
     </div>
   );
 }
+
+export default Home;
